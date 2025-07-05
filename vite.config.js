cb535861df58df669da5270eca759e146b4120d2
MackleeGit <mackleework@gmail.com>
2025-07-05 10:40:25 +0300

    feat(pwa): add PWA support with manifest, icons, and service worker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'favicon.svg',
+    
+      ],
+      manifest: {
+        name: 'STC Preorder System',
+        short_name: 'STC',
+        description: 'Order food before you arrive!',
+        theme_color: '#38bdf8',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
